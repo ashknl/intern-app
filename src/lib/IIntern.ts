0fd030ee9e4f0ea8c93c062d2f426756c0996871
ashknl <ash@ashkn.com>
2026-05-24 22:25:40 +0530

    added database and better-sqlite

---
+++ src/lib/IIntern.ts
@@ -0,0 +1,12 @@
+export interface Intern {
+    name: string;
+    institution_roll: string;
+    guardian_name: string;
+    guardian_relation: string;
+    branch: string;
+    year_of_study: string;
+    starting_date: Date,
+    no_of_days: number;
+    section_posted: string;
+    institution_name: string;
+}
